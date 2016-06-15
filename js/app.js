a59a6f042d39419133d41310c68b3bf9bc4f2311
lietu <411566654@qq.com>
2016-06-15 09:51:53 +0800

    ajax

--- js/app.js
+++ js/app.js
@@ -15,31 +15,10 @@ function bgMove(obj){
 	}
 }
 
-//选项条按钮点击效果切换
-function btnSwich(btnDown,btnUp){
-	var btnColor='#4E8BE6';//按钮选中颜色
-	$(btnUp).css('background','white').css('color','#666');
-	$(btnDown).css('background',btnColor).css('color','white');
-}
 
 //main
 $(function(){
 
-	//选项条按钮点击效果切换
-	$('.sell_btn').on('click',function(){
-		btnSwich('.sell_btn','.buy_btn');
-	});
-	$('.buy_btn').click(function(){
-		btnSwich('.buy_btn','.sell_btn');
-	});
-	$('#unlimited').change(function(){
-		if ($(this).is(':checked')) {
-			$('#region').attr('disabled','true').css({'border-color':'#ddd','color':'#bbb'});
-			$('#region').val("地区");
-		}else{
-			$('#region').removeAttr('disabled').css({'border-color':'#ccc','color':'#666'});
-		}
-	});
 	
 	//ajax关键词分页
 	var j=0;
@@ -47,10 +26,10 @@ $(function(){
 		$('.product_keyword li').eq(j).click(function(){
 			ajax('json/product_keyword'+($(this).index()+1)+'.json',function(srt){
 				var aData=eval(srt);
-				$('.product_cont li').each(function(){
-					$('.product_cont img').eq($(this).index()).attr('src',aData[$(this).index()].src);
-					$('.product_cont h3').eq($(this).index()).html(aData[$(this).index()].h3);
-					$('.product_cont p').eq($(this).index()).html(aData[$(this).index()].p);
+				$('.product_list .shop .wares').each(function(){
+					$('.product_list .shop .wares img').eq($(this).index()).attr('src',aData[$(this).index()].src);
+					$('.product_list .shop .wares h3').eq($(this).index()).html(aData[$(this).index()].h3);
+					$('.product_list .shop .wares p').eq($(this).index()).html(aData[$(this).index()].p);
 				})
 			})
 		})
